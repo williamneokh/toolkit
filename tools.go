415fa6491d8f75d4e3517247c0aab4ccb89737b6
williamneokh <75356102+williamneokh@users.noreply.github.com>
2023-01-04 22:20:42 +0800

    set characters

--- tools.go
+++ tools.go
@@ -11,7 +11,7 @@ import (
 	"strings"
 )
 
-const randomStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_+"
+const randomStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 // Tools is the type used to instantiate this module. Any variable of this type will have access
 // to all the methods with the reciever *Tools
